1987052cfa743c2de9a7334c024de58b708e4335
bkcatton <ben.k.catton@gmail.com>
2022-01-23 17:17:06 -0800

    Made the breedFetcher code work.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,22 @@
 const request = require('request');
 const userInput = process.argv[2];
+
 request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput}`, function (error, response, body) {
-  console.log('statusCode:', response && response.statusCode);
-  if(response) {
-    const data = JSON.parse(body);
-    if(data[0] === undefined) {
-      console.log("Breed Not Found");
-      return
-    }
-    console.log(data[0]['description']);
+  
+  if (error !== null) {
+    console.log("Error with request: ", error['code'], "at ", error['hostname']);
     return
   }
-  if (error['code'] === "ENOTFOUND") {
-    console.log("Webpage was not loaded correctly");
-    return;
+  // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  
+  if (body === '[]') {
+    console.log("that search didnt work");
+    return
   }
+
+  const data = JSON.parse(body);
+  console.log(data[0]['description']);
+  console.log(typeof data);
+
 });
